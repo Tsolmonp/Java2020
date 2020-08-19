d3d17a1fdf8fd18f600204d2c91e00f275e73174
TsolmonP <tsolp0123@gmail.com>
2020-08-18 19:24:07 -0500

    class

---
+++ src/Day43_Static/CybertekStudent.java
@@ -0,0 +1,18 @@
+package Day43_Static;
+
+class A{
+    static int a = 200;
+}
+
+public class CybertekStudent {
+    //for the advantages of static
+
+    public static void main(String[] args) {
+
+        String studentName;
+        int age;
+        char gender;
+
+        static String schoolName = "Cybertek School";
+    }
+}
