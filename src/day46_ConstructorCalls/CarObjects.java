b8ebe2b7bd2bd7b8b8af020a50cfe2d200def7e1
TsolmonP <tsolp0123@gmail.com>
2020-08-26 20:54:04 -0500

    day_46

---
+++ src/day46_ConstructorCalls/CarObjects.java
@@ -0,0 +1,22 @@
+package day46_ConstructorCalls;
+
+public class CarObjects {
+    public static void main(String[] args) {
+        System.out.println(new Car ("Toyota"));
+
+        System.out.println("==========================");
+        System.out.println(new Car("BMW","XS"));
+
+        System.out.println("==============================");
+
+        System.out.println(new Car("Audi","Q7","White"));
+
+        System.out.println("======================================");
+
+        System.out.println(new Car("Jeep","Compass","Black",2010));
+
+        System.out.println("======================================");
+
+
+    }
+}
