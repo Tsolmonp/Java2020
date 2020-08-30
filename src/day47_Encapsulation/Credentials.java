41a1dc37c340e8e3903929b1115567abb9c0c53d
TsolmonP <tsolp0123@gmail.com>
2020-08-30 09:05:05 -0500

    day_47

---
+++ src/day47_Encapsulation/Credentials.java
@@ -0,0 +1,43 @@
+package day47_Encapsulation;
+/*
+public String name;
+public int age;
+private String username;
+private String password;
+
+ */
+public class Credentials{
+    public String name;
+    public int age;
+
+    public static String companyName;
+
+    private String userName;
+    private String passWord;
+
+    static{
+        companyName = "FaceBook";
+    }
+
+    public Credentials(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+
+    public void setUserName(String userName){
+        this.userName = userName;
+    }
+
+    public void setPassWord(String passWord){
+        this.passWord = passWord;
+    }
+
+    public String getUserName(){
+        return userName;
+    }
+
+    public String getPassWord(){
+        return passWord;
+    }
+
+}
---
+++ src/day47_Encapsulation/Test_AccessModifiers.java
@@ -0,0 +1,11 @@
+package day47_Encapsulation;
+import day47_Encapsulation.AccessModifiers;
+
+public class Test_AccessModifiers {
+
+    public static void main(String[] args) {
+
+        System.out.println(AccessModifiers.publicVariable);
+    }
+
+}
