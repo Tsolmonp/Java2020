8414b0fa48af055aec3fde51a640fee663dd7ffb
TsolmonP <tsolp0123@gmail.com>
2020-09-19 09:24:33 -0500

    day56

---
+++ src/day56_Abstraction/PhoneTask/Samsung.java
@@ -0,0 +1,28 @@
+package day56_Abstraction.PhoneTask;
+
+public class Samsung extends  Phone {
+
+    public Samsung(String brand, String model, double price, String size) {
+        super(brand, model, price, size);
+    }
+
+    @Override
+    public void calling(long phoneNumber) {
+        System.out.println("Samsung is calling "+phoneNumber);
+    }
+
+    @Override
+    public void texting(long phoneNumber) {
+        System.out.println("Samsung is texting "+phoneNumber);
+    }
+
+    @Override
+    public String toString() {
+        return "Samsung{" +
+                "brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                ", price= $" + price +
+                ", size='" + size + '\'' +
+                '}';
+    }
+}
