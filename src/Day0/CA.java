bbc2bb3357346addc239db2605a45df07ae2e72c
TsolmonP <tsolp0123@gmail.com>
2020-11-04 17:11:55 -0600

    office hour Nov

---
+++ src/Day0/CA.java
@@ -0,0 +1,28 @@
+package Day0;
+
+import java.util.Arrays;
+
+public class CA {
+    public static void main(String[] args) {
+
+
+        int num1 = 9;
+        if(++num1<10){
+            System.out.println(num1 + "HW");
+        }else{
+            System.out.println(num1+ "HU");
+        }
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/Office_Hours/Practice_11_4_2020/Topping3.java
@@ -0,0 +1,18 @@
+package Office_Hours.Practice_11_4_2020;
+
+import java.util.Map;
+
+public class Topping3 {
+
+    public Map<String, String> topping3(Map<String, String> map) {
+
+        if (!map.get("potato").isEmpty()) {
+            map.put("fries", map.get("potato"));
+        }
+        if (map.get("salad") != null) {
+            map.put("spinach", map.get("salad"));
+        }
+        return map;
+    }
+
+}
