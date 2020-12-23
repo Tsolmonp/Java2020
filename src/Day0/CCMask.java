521836e43f1c97cbe44a0fee84fc7d829d082a6d
TsolmonP <tsolp0123@gmail.com>
2020-12-23 17:00:47 -0600

    Office hour with Saim

---
+++ src/Day0/CCMask.java
@@ -0,0 +1,29 @@
+package Day0;
+import Day01.p1;
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+import org.w3c.dom.ls.LSOutput;
+
+
+public class CCMask {
+
+
+
+    public static void main(String[] args) {
+        int iVar = 100;
+        float fVar = 100.100f;
+        double dVar = 123;
+       /** iVar = fVar;
+        fVar =iVar;
+        dVar = fVar;
+        fVar = dVar;
+        dVar = iVar;
+        iVar = dVar;
+
+*/
+
+
+
+
+        }
+
+        }
---
+++ src/Day01/p1.java
@@ -0,0 +1,10 @@
+package Day01;
+
+public class p1 {
+    public class Acc{
+        int p;
+        private int q;
+        protected int r;
+        public int s;
+    }
+}
